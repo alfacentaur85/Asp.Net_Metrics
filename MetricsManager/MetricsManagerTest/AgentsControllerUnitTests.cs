5f5e53e148c41e3f4a41ea646e4e660d45238b81
alfacentaur <alfacentaur@yandex.ru>
2021-04-30 11:27:41 +0300

    Lesson2_changed
    Lesson2_changed

---
+++ MetricsManager/MetricsManagerTest/AgentsControllerUnitTests.cs
@@ -0,0 +1,64 @@
+﻿using MetricsManager.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using Xunit;
+using MetricsManager.Enums;
+using MetricsManager;
+
+namespace MetricsManagerTest
+{
+    public class AgentsControllerUnitTests
+    {
+        private AgentsController _controller;
+
+        public AgentsControllerUnitTests()
+        {
+            _controller = new AgentsController();
+        }
+
+        [Fact]
+        public void RegisterAgent_ReturnsOK()
+        {
+            //Arrange
+            var agentInfo = new AgentInfo();
+
+
+            //Act
+            var result = _controller.RegisterAgent(agentInfo);
+
+            //Assert
+            _ = Assert.IsAssignableFrom<IActionResult>(result);
+        }
+
+
+        [Fact]
+        public void EnableAgentById_ReturnsOK()
+        {
+            //Arrange
+            var agentId = 1;
+
+
+            //Act
+            var result = _controller.EnableAgentById(agentId);
+
+            //Assert
+            _ = Assert.IsAssignableFrom<IActionResult>(result);
+        }
+
+        [Fact]
+        public void DisableAgentById_ReturnsOK()
+        {
+            //Arrange
+            var agentId = 1;
+
+
+            //Act
+            var result = _controller.DisableAgentById(agentId);
+
+            //Assert
+            _ = Assert.IsAssignableFrom<IActionResult>(result);
+        }
+
+    }
+}
+
---
+++ MetricsManager/MetricsManagerTest/RamMetricsControllerUnitTests.cs
@@ -0,0 +1,79 @@
+﻿using MetricsManager.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using Xunit;
+using MetricsManager.Enums;
+
+namespace MetricsManagerTest
+{
+    public class RamControllerUnitTests
+    {
+        private RamMetricsController _controller;
+
+        public RamControllerUnitTests()
+        {
+            _controller = new RamMetricsController();
+        }
+
+        [Fact]
+        public void GetMetricsFromAgent_ReturnsOK()
+        {
+            //Arrange
+            var agentId = 1;
+            var fromTime = TimeSpan.FromSeconds(0);
+            var toTime = TimeSpan.FromSeconds(100);
+
+            //Act
+            var result = _controller.GetMetricsFromAgent(agentId, fromTime, toTime);
+
+            //Assert
+            _ = Assert.IsAssignableFrom<IActionResult>(result);
+        }
+
+        [Fact]
+        public void GetMetricsByPercentileFromAgent_ReturnsOK()
+        {
+            //Arrange
+            var agentId = 1;
+            var fromTime = TimeSpan.FromSeconds(0);
+            var toTime = TimeSpan.FromSeconds(100);
+
+            //Act
+            var result = _controller.GetMetricsByPercentileFromAgent(agentId, fromTime, toTime);
+
+            //Assert
+            _ = Assert.IsAssignableFrom<IActionResult>(result);
+        }
+
+        [Fact]
+        public void GetMetricsFromAllCluster_ReturnsOK()
+        {
+            //Arrange
+            var fromTime = TimeSpan.FromSeconds(0);
+            var toTime = TimeSpan.FromSeconds(100);
+
+            //Act
+            var result = _controller.GetMetricsFromAllCluster(fromTime, toTime);
+
+            //Assert
+            _ = Assert.IsAssignableFrom<IActionResult>(result);
+        }
+
+        [Fact]
+        public void GetMetricsByPercentileFromAllCluster_GetMetricsFromAllCluster_ReturnsOK()
+        {
+            //Arrange
+            var fromTime = TimeSpan.FromSeconds(0);
+            var toTime = TimeSpan.FromSeconds(100);
+            Percentile percentiles = Percentile.P75;
+
+            //Act
+            var result = _controller.GetMetricsByPercentileFromAllCluster(fromTime, toTime, percentiles);
+
+            //Assert
+            _ = Assert.IsAssignableFrom<IActionResult>(result);
+        }
+
+
+    }
+}
