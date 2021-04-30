a6f661a0757f481861b9f89002a322bd43200966
alfacentaur <alfacentaur@yandex.ru>
2021-04-30 11:16:00 +0300

    Lesson2_changed
    Lesson2_changed

--- MetricsManager/MetricsAgent/Startup.cs
+++ MetricsManager/MetricsAgent/Startup.cs
@@ -12,7 +12,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace MetricsManager
+namespace MetricsAgent
 {
     public class Startup
     {
@@ -26,11 +26,11 @@ namespace MetricsManager
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            
+
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "MetricsManager", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "MetricsAgent", Version = "v1" });
             });
         }
 
@@ -41,7 +41,7 @@ namespace MetricsManager
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "MetricsManager v1"));
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "MetricsAgent v1"));
             }
 
             app.UseHttpsRedirection();
---
+++ MetricsManager/MetricsAgentTest/DotNetMetricsAgentControllerUnitTests.cs
@@ -0,0 +1,33 @@
+﻿using MetricsAgent.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using Xunit;
+
+namespace MetricsManagerTest
+{
+    public class DotNetControllerUnitTests
+    {
+        private DotNetMetricsAgentController _controller;
+
+        public DotNetControllerUnitTests()
+        {
+            _controller = new DotNetMetricsAgentController();
+        }
+
+        [Fact]
+        public void GetMetricsFromAgent_ReturnsOK()
+        {
+            //Arrange
+            var fromTime = TimeSpan.FromSeconds(0);
+            var toTime = TimeSpan.FromSeconds(100);
+
+            //Act
+            var result = _controller.GetMetricsFromAgent(fromTime, toTime);
+
+            //Assert
+            _ = Assert.IsAssignableFrom<IActionResult>(result);
+        }
+
+    }
+}
+
