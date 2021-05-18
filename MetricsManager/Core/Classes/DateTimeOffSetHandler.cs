61797d502df031d9cf87414d1091fde60482f695
Artur Stepanyants <alfacentaur@yandex.ru>
2021-05-18 19:09:42 +0300

    Lesson5_mod1
    Lesson5_mod1

---
+++ MetricsManager/Core/Classes/DateTimeOffSetHandler.cs
@@ -0,0 +1,17 @@
+﻿using Dapper;
+using System.Data;
+using System;
+
+
+namespace Core
+{
+    public class DateTimeOffSetHandler : SqlMapper.TypeHandler<DateTimeOffset>
+    {
+        public override DateTimeOffset Parse(object value)
+            => DateTimeOffset.FromUnixTimeSeconds((Int32)value);
+
+        public override void SetValue(IDbDataParameter parameter, DateTimeOffset value)
+            => parameter.Value = value;
+    }
+
+}
---
+++ MetricsManager/MetricsManagerTest/DotNetCotrollerUnitTests.cs
@@ -0,0 +1,51 @@
+﻿using MetricsManager.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using Xunit;
+using Microsoft.Extensions.Logging;
+
+namespace MetricsManagerTest
+{
+    public class DotNetControllerUnitTests
+    {
+        private DotNetMetricsController _controller;
+
+        private readonly ILogger<DotNetMetricsController> _logger = new Microsoft.Extensions.Logging.LoggerFactory().CreateLogger<DotNetMetricsController>();
+
+
+        public DotNetControllerUnitTests()
+        {
+            _controller = new DotNetMetricsController(_logger);
+        }
+
+        [Fact]
+        public void GetMetricsFromAgent_ReturnsOK()
+        {
+            //Arrange
+            var agentId = 1;
+            var fromTime = DateTimeOffset.FromUnixTimeSeconds(0);
+            var toTime = DateTimeOffset.FromUnixTimeSeconds(100);
+
+            //Act
+            var result = _controller.GetMetricsFromAgent(agentId, fromTime, toTime);
+
+            //Assert
+            _ = Assert.IsAssignableFrom<IActionResult>(result);
+        }
+
+        [Fact]
+        public void GetMetricsFromAllCluster_ReturnsOK()
+        {
+            //Arrange
+            var fromTime = DateTimeOffset.FromUnixTimeSeconds(0);
+            var toTime = DateTimeOffset.FromUnixTimeSeconds(100);
+
+            //Act
+            var result = _controller.GetMetricsFromAllCluster(fromTime, toTime);
+
+            //Assert
+            _ = Assert.IsAssignableFrom<IActionResult>(result);
+        }
+
+    }
+}
